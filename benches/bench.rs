10faf61bbed6e1aad8696adbedb839dadac781f0
Tim Robinson <tim.g.robinson@gmail.com>
2018-11-15 07:27:04 +0000

    Add benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,42 +9,42 @@ use std::collections::BTreeMap;
 use top_map::TopMap;
 
 fn bench(c: &mut Criterion) {
-    fn insert_small_top_map(b: &mut Bencher, &index: &isize) {
+    fn insert_remove_empty_top_map(b: &mut Bencher, &index: &isize) {
         let mut m = TopMap::new(100);
         m.reserve(1);
 
         b.iter(|| {
-            m.remove(index);
             m.insert(index, index);
+            m.remove(index);
         });
     }
 
-    fn insert_small_btree_map(b: &mut Bencher, &index: &isize) {
+    fn insert_remove_empty_btree_map(b: &mut Bencher, &index: &isize) {
         let mut m = BTreeMap::new();
 
         b.iter(|| {
-            m.remove(&index);
             m.insert(index, index);
+            m.remove(&index);
         });
     }
 
-    fn insert_big_top_map(b: &mut Bencher, &index: &isize) {
+    fn insert_remove_existing_top_map(b: &mut Bencher, &index: &isize) {
         let mut m = TopMap::new(100);
         m.extend((0..1000).map(|n| (n as isize, n)));
 
         b.iter(|| {
-            m.remove(index);
             m.insert(index, index);
+            m.remove(index);
         });
     }
 
-    fn insert_big_btree_map(b: &mut Bencher, &index: &isize) {
+    fn insert_remove_existing_btree_map(b: &mut Bencher, &index: &isize) {
         let mut m = BTreeMap::new();
         m.extend((0..1000).map(|n| (n as isize, n)));
 
         b.iter(|| {
-            m.remove(&index);
             m.insert(index, index);
+            m.remove(&index);
         });
     }
 
@@ -84,17 +84,17 @@ fn bench(c: &mut Criterion) {
         });
     }
 
-    for &n in [0, 99, 100, 500, 999].iter() {
+    for &n in [0, 50, 99, 100, 500, 999].iter() {
         c.bench_functions(
             &n.to_string(),
             vec![
-                Fun::new("insert_small_top_map", insert_small_top_map),
-                Fun::new("insert_small_btree_map", insert_small_btree_map),
-                Fun::new("insert_big_top_map", insert_big_top_map),
-                Fun::new("insert_big_btree_map", insert_big_btree_map),
+                Fun::new("insert_remove_empty_top_map", insert_remove_empty_top_map),
+                Fun::new("insert_remove_existing_top_map", insert_remove_existing_top_map),
                 Fun::new("lookup_top_map", lookup_top_map),
-                Fun::new("lookup_btree_map", lookup_btree_map),
                 Fun::new("increment_top_map", increment_top_map),
+                Fun::new("insert_remove_empty_btree_map", insert_remove_empty_btree_map),
+                Fun::new("insert_remove_existing_btree_map", insert_remove_existing_btree_map),
+                Fun::new("lookup_btree_map", lookup_btree_map),
                 Fun::new("increment_btree_map", increment_btree_map),
             ],
             n,
